5388d9affd650f38e6487fd004410bc7ff6c80bf
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2023-03-12 18:13:45 +0100

    reate deploy workflow

--- deploy.sh
+++ deploy.sh
@@ -8,22 +8,23 @@ GIT_FTP_DIR="git-ftp/dist"
 SOURCE_COMMIT=`git log --format="%H" -n 1`
 
 echo "Pushing dist folder $OUTPUT_DIR to dist repository $DIST_REPO_NAME"
-git config --global user.name "Travis CI"
-git config --global user.email "travis@travis-ci.org"
+git config --global user.name "github deloy workflow"
+git config --global user.email "workflow@example.com"
 
-DIST_REPO=https://${GH_TOKEN:-git}@github.com/${DIST_REPO_NAME}.git
+DIST_REPO=https://x-access-token:${GH_TOKEN:-git}@github.com/${DIST_REPO_NAME}
 mkdir $DIST_REPO_DIR
 cd $DIST_REPO_DIR
 git init
-git pull --depth 30 "$DIST_REPO"
+git pull --depth 30 "$DIST_REPO.git"
 rsync -r --exclude=.git --delete ../$OUTPUT_DIR/ ./
 
 git add -A
 git status -s
 
 git commit -m "generate site from ${SOURCE_REPO_NAME}@${SOURCE_COMMIT}"
-git push -q "$DIST_REPO" master
-../$GIT_FTP_DIR/git-ftp push -u $FTP_USER -p $FTP_PASSWORD $FTP_URL
+git remote add origin "$DIST_REPO"
+git push -q origin master
+../$GIT_FTP_DIR/git-ftp push -u $FTP_USER -p $FTP_PASSWORD -v $FTP_URL 
 
 cd -
 
--- install-git-ftp.sh
+++ install-git-ftp.sh
@@ -1,4 +1,4 @@
-GIT_FTP_VERSION="1.5.1"
+GIT_FTP_VERSION="1.6.0"
 git clone https://github.com/git-ftp/git-ftp.git git-ftp
 cd git-ftp
 git checkout $GIT_FTP_VERSION
